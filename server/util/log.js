ace0d73a435a66db993a32cab351144ebe4f9b4b
DynamikArray <DynamikArray@gmail.com>
2021-04-09 16:46:54 -0400

    Colorize log files in winston

--- server/util/log.js
+++ server/util/log.js
@@ -1,5 +1,12 @@
 const winston = require("winston");
+const colorizer = winston.format.colorize();
+
 const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.simple(),
+    winston.format.printf((msg) => colorizer.colorize(msg.level, `${msg.timestamp} - ${msg.level}: ${msg.message}`))
+  ),
   transports: [new winston.transports.Console()],
 });
 
